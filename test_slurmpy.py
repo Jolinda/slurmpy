2deb29354407ee333e57b0d3f81d33ed54963ed6
jolinda <jolinda@uoregon.edu>
2022-06-06 17:01:13 -0700

    adding more unit tests

--- test_slurmpy.py
+++ test_slurmpy.py
@@ -11,7 +11,7 @@ class TestSlurmJobWrap(unittest.TestCase):
 
     def test_wrap_with_args(self):
         sj = slurmpy.SlurmJob(command='echo Hello World', jobname='testwrap', account='lcni',
-                              partition='interactive')
+                              partition='interactive', output_directory='slurm_out')
         sj.wrap_slurm_command()
         self.assertIsNotNone(sj.jobid)
 
@@ -49,6 +49,18 @@ class TestSlurmJobFile(unittest.TestCase):
         sj.submit_slurm_file()
         self.assertIsNotNone(sj.jobid)
 
+    def test_array_with_args(self):
+        sj = slurmpy.SlurmJob()
+        sj.command = 'echo Hello $x'
+        sj.jobname = 'helloarray'
+        sj.array = ['Eugene', 'Springfield', 'Oregon', 'USA', 'World']
+        sj.output_directory = 'slurm_out'
+        sj.partition = 'short'
+        sj.write_slurm_file()
+        sj.print_slurm_file()
+        sj.submit_slurm_file()
+        self.assertIsNotNone(sj.jobid)
+
     # test without submitting a new job; temporary
     def test_output(self):
         sj = slurmpy.SlurmJob(jobname='helloworld', jobid='20627824')
@@ -60,6 +72,24 @@ class TestSlurmJobFile(unittest.TestCase):
     # test without submitting a new job; temporary
     def test_output_array(self):
         sj = slurmpy.SlurmJob(jobname='helloarray', jobid='20628519')
+        files = sj.get_output_files()
+        print(files)
+        self.assertTrue(files)
+        sj.show_output()
+
+    # test without submitting a new job; temporary
+    def test_output_dir(self):
+        sj = slurmpy.SlurmJob(jobname='testwrap', jobid='20775614')
+        sj.output_directory = 'slurm_out'
+        files = sj.get_output_files()
+        print(files)
+        self.assertTrue(files)
+        sj.show_output()
+
+    # test without submitting a new job; temporary
+    def test_output_array_dir(self):
+        sj = slurmpy.SlurmJob(jobname='helloarray', jobid='20775995')
+        sj.output_directory = 'slurm_out'
         files = sj.get_output_files()
         print(files)
         self.assertTrue(files)
